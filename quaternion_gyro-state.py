246b65980b9dc24786016bdfa96e7c6f5f59a64d
Matthew Ricci <s3785111@student.rmit.edu.au>
2024-05-15 21:36:44 +1000

    Update quaternion calculation script to be more generalised, support command line arguments for passing in parameters

--- quaternion_gyro-state.py
+++ quaternion_gyro-state.py
@@ -3,32 +3,8 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import math
+import argparse
 
-###########################################################################
-#                            INITILISATION                                #
-###########################################################################
-
-data = pd.read_csv('./csv/A1/data_highres.csv')
-data_br = pd.read_csv('./csv/A1/data_raven_highres.csv')
-
-# Constants and data preparation
-data_count = len(data['sync'])
-dt = 0.004  # Time interval between measurements
-sens = 13.375
-
-t = [x*dt for x in range(data_count)]
-t_br = data_br['Flight_Time_(s)']
-
-gyro = [
-    [x / sens for x in data['gyro_x']],
-    [y / sens for y in data['gyro_y']],
-    [z / sens for z in data['gyro_z']],
-]
-gyro_br = [
-    [x for x in data_br['Gyro_X']],
-    [x for x in data_br['Gyro_Y']],
-    [x for x in data_br['Gyro_Z']]
-]
 
 ###########################################################################
 #                       QUATERNION METHODS                                #
@@ -36,7 +12,6 @@ gyro_br = [
 
 # Yoinked from:
 # https://www.rocketryforum.com/threads/gyros-tilt-and-quaternions-are-not-your-friend.164151/
-
 
 class Quaternion:
     def __init__(self, x=0.0, y=0.0, z=0.0, r=1.0):
@@ -104,8 +79,8 @@ def quaternion_init_half_euler(g_roll, g_pitch, g_yaw):
     Returns:
         The initialized quaternion.
     """
-    x = (g_roll / 2.0) * math.pi / \
-        180.0  # Half the degree and convert it to radians
+    # Half the degree and convert it to radians
+    x = (g_roll / 2.0) * math.pi / 180.0
     y = (g_pitch / 2.0) * math.pi / 180.0
     z = (g_yaw / 2.0) * math.pi / 180.0
 
@@ -129,98 +104,162 @@ def quaternion_init_half_euler(g_roll, g_pitch, g_yaw):
 ###########################################################################
 
 # Integrate gyroscope data with bias
-quats = [Quaternion(0, 0, 0, 1)]
-for i in range(data_count):
-    gyro_x = gyro[0][i]
-    gyro_y = gyro[1][i]
-    gyro_z = gyro[2][i]
+def calculate_quat(gyro, dt):
+    quats = [Quaternion(0, 0, 0, 1)]
+    for i in range(len(gyro[0])):
+        gyro_x = gyro[0][i]
+        gyro_y = gyro[1][i]
+        gyro_z = gyro[2][i]
 
-    q_dot = quaternion_init_half_euler(
-        dt * (gyro_x),
-        dt * (gyro_y),
-        dt * (gyro_z)
+        q_dot = quaternion_init_half_euler(
+            dt * (gyro_x),
+            dt * (gyro_y),
+            dt * (gyro_z)
+        )
+
+        q_w = quaternion_multiply(quats[i], q_dot)
+        quats.append(quaternion_normalize(q_w))
+
+    quats = [(q.x, q.y, q.z, q.r) for q in quats]
+    np.savetxt(
+        './csv/quaternion_estimate.csv',
+        quats, delimiter=',', header="x,y,z,w"
     )
+    return quats
 
-    q_w = quaternion_multiply(quats[i], q_dot)
-    quats.append(quaternion_normalize(q_w))
-
-quat_br = list(zip(
-    data_br["Quat_1"],
-    data_br["Quat_2"],
-    [-x for x in data_br["Quat_4"]],
-    data_br["Quat_3"],
-))
-np.savetxt('./csv/quaternion_estimate.csv', [
-    (q.x, q.y, q.z, q.r) for q in quats
-], delimiter=',', header="x,y,z,w")
 
 ###########################################################################
 #                            VISUALISATION                                #
 ###########################################################################
 
-# ESTIMATES
-# ----------------------------------------------------------------
-# Some components are swapped compared to raven ground truth, why?
-# For estimate->ground truth:
-#   w->x
-#   x->-w
-#   y->z
-#   z->-y
-start_time = 0
-duration = 11
+def visualise(quats, start_time, duration, dt, typestr):
 
-plt.figure()
-plt.plot(t[int(start_time/dt):int(duration/dt)],
-         [q.r for q in quats[int(start_time/dt):int(duration/dt)]])
-plt.plot(t[int(start_time/dt):int(duration/dt)],
-         [q.x for q in quats[int(start_time/dt):int(duration/dt)]])
-plt.plot(t[int(start_time/dt):int(duration/dt)],
-         [q.y for q in quats[int(start_time/dt):int(duration/dt)]])
-plt.plot(t[int(start_time/dt):int(duration/dt)],
-         [q.z for q in quats[int(start_time/dt):int(duration/dt)]])
-plt.xlabel("Time (s)")
-plt.title("Quaternion estimates")
-plt.legend(["w", "x", "y", "z"], loc="upper left")
-plt.savefig("./plot/state/attitude/quat_estimate.png")
+    t = [dt*i-start_time for i in range(len(quats))]
 
-# EULER ANGLE
-plt.figure()
-plt.plot(t[int(start_time/dt):int(duration/dt)],
-         [
-    Rotation.from_quat(q.as_list()).as_euler("xyz", degrees=True)
-    for q in quats[int(start_time/dt):int(duration/dt)]
-])
-plt.xlabel("Time (s)")
-plt.ylabel("Angle (degrees)")
-plt.title("Euler angle estimates")
-plt.legend(["Roll", "Pitch", "Yaw"], loc="lower right")
-plt.savefig("./plot/state/attitude/euler_estimate.png")
+    plt.figure()
+    plt.plot(t[int(start_time/dt):int(duration/dt)],
+             quats[int(start_time/dt):int(duration/dt)])
+    plt.xlabel("Time (s)")
+    plt.title(f"Quaternion {typestr}")
+    plt.legend(["x", "y", "z", "w"], loc="upper left")
+    plt.savefig(f"./plot/state/attitude/quat_{typestr}.png")
 
-# GROUND TRUTH
-# ----------------------------------------------------------------
-start_time = 2
-duration += start_time
-dt = 0.002
+    # EULER ANGLE
+    plt.figure()
+    plt.plot(t[int(start_time/dt):int(duration/dt)],
+             [
+        Rotation.from_quat(q).as_euler("xyz", degrees=True)
+        for q in quats[int(start_time/dt):int(duration/dt)]
+    ])
+    plt.xlabel("Time (s)")
+    plt.ylabel("Angle (degrees)")
+    plt.title(f"Euler angle {typestr}")
+    plt.legend(["Roll", "Pitch", "Yaw"], loc="lower right")
+    plt.savefig(f"./plot/state/attitude/euler_{typestr}.png")
+    plt.show()
 
-plt.figure()
-plt.plot(t_br[int(start_time/dt):int(duration/dt)],
-         quat_br[int(start_time/dt):int(duration/dt)])
-plt.xlabel("Time (s)")
-plt.title("Quaternion ground truth")
-plt.legend(["w", "x", "y", "z"], loc="upper left")
-plt.savefig("./plot/state/attitude/quat_truth.png")
 
-# EULER ANGLE
-plt.figure()
-plt.plot(t_br[int(start_time/dt):int(duration/dt)],
-         [
-    Rotation.from_quat(q).as_euler("xyz", degrees=True)
-    for q in quat_br[int(start_time/dt):int(duration/dt)]
-])
-plt.xlabel("Time (s)")
-plt.ylabel("Angle (degrees)")
-plt.title("Euler angle estimates")
-plt.legend(["Roll", "Pitch", "Yaw"], loc="lower right")
-plt.savefig("./plot/state/attitude/euler_truth.png")
+if __name__ == '__main__':
 
-plt.show()
+    ###########################################################################
+    #                            INITILISATION                                #
+    ###########################################################################
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-f', '--freq',
+                        metavar='freqAV:freqBR',
+                        type=str,
+                        help='Frequency range in the format freq1:freq2',
+                        required=True)
+
+    parser.add_argument('-t', '--time',
+                        metavar='{AV/BR}:start:duration',
+                        type=str, nargs=2,
+                        help='Time ranges for AV and BR (e.g., AV:0:5 BR:10:15)',
+                        required=True)
+
+    parser.add_argument('--axisAV',
+                        metavar='axes[scalars,]',
+                        type=str,
+                        help='Axis adjustments for AV measurements',
+                        default='xyz[1,1,1]')
+
+    parser.add_argument('--axisBR',
+                        metavar='axes[scalars,]',
+                        type=str,
+                        help='Axis adjustments for BR measurements',
+                        default='xyz[1,1,1]')
+
+    parser.add_argument('directory',
+                        metavar='directory',
+                        type=str,
+                        help='Path to the directory to extract CSV data from')
+    args = parser.parse_args()
+
+    # Extract CSV data from relevant directory
+    directory = args.directory
+    data_AV = pd.read_csv(f'./csv/{directory}/data_highres.csv')
+    data_BR = pd.read_csv(f'./csv/{directory}/data_raven_highres.csv')
+
+    # Axis adjustments
+    # -----------------------------------------------------------------
+    # This is an absolutely devilish and disgusting hacky workaround to
+    # adjusting the axes and their scaling
+
+    # Rotate AV data to global frame
+    ax_AV = args.axisAV[:3]
+    scale_x, scale_y, scale_z = map(
+        float, args.axisAV[4:-1].split(','))    # I want to vomit
+
+    sens = 13.375
+    gyro_AV = [
+        [scale_x * x / sens for x in data_AV[f'gyro_{ax_AV[0]}']],
+        [scale_y * y / sens for y in data_AV[f'gyro_{ax_AV[1]}']],
+        [scale_z * z / sens for z in data_AV[f'gyro_{ax_AV[2]}']],
+    ]
+
+    # Rotate BR data to global frame
+    ax_BR = args.axisBR[:3]
+    scale_x, scale_y, scale_z = map(
+        float, args.axisBR[4:-1].split(','))    # Yuck
+
+    gyro_BR = [
+        [scale_x * x for x in data_BR[f'Gyro_{ax_BR[0]}']],
+        [scale_y * y for y in data_BR[f'Gyro_{ax_BR[1]}']],
+        [scale_z * z for z in data_BR[f'Gyro_{ax_BR[2]}']]
+    ]
+
+    # Time parsing
+    # -------------------------------------------------------
+    time_ranges = {}
+    for time_str in args.time:
+        source, start, duration = time_str.split(':')
+        time_ranges[source] = (float(start), float(duration))
+
+    ###########################################################################
+    #                              EXECUTION                                  #
+    ###########################################################################
+
+    # Visualise AV estimates
+    start_AV, duration_AV = time_ranges['AV']
+    freq = int(args.freq.split(':')[0])
+    dt = 1/freq
+    visualise(calculate_quat(gyro_AV, dt), start_AV,
+              duration_AV, dt, "AV estimates")
+
+    # Visualise BR estimates
+    start_BR, duration_BR = time_ranges['BR']
+    freq = int(args.freq.split(':')[1])
+    dt = 1/freq
+    visualise(calculate_quat(gyro_BR, dt), start_BR,
+              duration_BR, dt, "BR estimates")
+
+    # Manually rotating BR truth quaternions to global frame (ew)
+    quats_truth = list(zip(
+        [-x for x in data_BR['Quat_4']],
+        [y for y in data_BR['Quat_3']],
+        [z for z in data_BR['Quat_2']],
+        [w for w in data_BR['Quat_1']],
+    ))
+    # Visualise BR truth
+    visualise(quats_truth, start_BR, duration_BR, dt, "truth")
