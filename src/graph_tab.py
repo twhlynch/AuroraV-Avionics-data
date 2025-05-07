ee81fba906ceb1e55678417f0e01d05f7dbd00a7
Tom Lynch <twhlynch@gmail.com>
2025-05-07 13:53:24 +1000

    reduce graphs font size

--- src/graph_tab.py
+++ src/graph_tab.py
@@ -19,6 +19,7 @@ class GraphTab(ttk.Frame):
 		self.setup()
 
 		parent.add(self, text=self.title)
+		plt.rcParams['font.size'] = 7
 		self.fig, self.ax = plt.subplots(figsize=(6, 4), dpi=80)
 
 		self.graph()
